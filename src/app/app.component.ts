bded7879aa21362831ef40a0708ccb6d6d215215
unknown <dean.rapson@ontario.ca>
2018-08-08 10:55:38 -0400

    only middle show on xs screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ObservableMedia, MediaChange } from "@angular/flex-layout";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'mu-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mu';
+  mobileMode: Observable<boolean>;
+
+  constructor(private media: ObservableMedia)
+  {
+    this.mobileMode = this.media.asObservable()
+      .pipe(map(MediaChange=>{
+            console.log(MediaChange.mqAlias);
+            return MediaChange.mqAlias!=='xs'? true: false;
+          }))
+  }
 }
